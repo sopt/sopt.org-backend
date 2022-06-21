dea76a338e0b999ed902fb5870b417bdb275ab2a
kmebin <blowhuiee@gmail.com>
2022-06-21 16:49:46 +0900

    [ADD] 프로젝트 초기 세팅

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, NextFunction } from "express";
+import config from "./config";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+interface ErrorType {
+  message: string;
+  status: number;
+}
+
+app.use(function (err: ErrorType, req: Request, res: Response, next: NextFunction) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "production" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app
+  .listen(config.port, () => {
+    console.log(`
+    ################################################
+          🛡️  Server listening on port: ${config.port} 🛡️
+    ################################################
+  `);
+  })
+  .on("error", err => {
+    console.error(err);
+    process.exit(1);
+  });
