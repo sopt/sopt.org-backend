c0788e5c94f575b33439a9415526a5ccff56adeb
KIM HEE BIN <blowhuiee@gmail.com>
2022-08-09 10:58:14 +0900

    [CHORE] 불필요한 폴더 제거

--- src/modules/logos/logos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LogosController } from './logos.controller';
-import { LogosService } from './logos.service';
-
-@Module({
-  providers: [LogosService],
-  controllers: [LogosController],
-})
-export class LogosModule {}
