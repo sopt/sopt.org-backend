107d22dbec3d8701d04d38b8cf93528819ddb111
kmebin <blowhuiee@gmail.com>
2022-08-09 14:12:39 +0900

    [UPDATE] description 추가

--- src/modules/history/history.controller.ts
+++ src/modules/history/history.controller.ts
@@ -15,6 +15,23 @@ export class HistoryController {
   @Get(':semesterId')
   @ApiOperation({
     summary: '기수별 연혁 조회',
+    description: `
+    기수별 회원 구성 중 파트별 인원이 없는 경우, 총 인원 수만 조회합니다. \n
+    [파트]의 id값입니다. (1-5 까지는 회장단입니다.) \n
+    1: 회장 \n
+    2: 부회장 \n
+    3: 총무 \n
+    4: 운영팀장 \n
+    5: 미디어팀장 \n
+    6: 기획파트장 \n
+    7: 디자인파트 \n
+    8: 안드로이드파트장 \n
+    9: iOS파트장 \n
+    10: 웹파트장 \n
+    11: 서버파트장 \n
+    12: 개발파트장 \n
+    13: 클라이언트파트장 \n
+    `,
   })
   @ApiOkResponse({
     description: '기수별 연혁 조회 성공',
